0cc820ae6e0bffdd860ccea0294b7cccc799717c
Nergal <a.poluhovich@openmedia.net>
2011-03-25 15:55:12 +0200

    Initial commit

---
+++ init.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+if ($path = Kohana::find_file('vendor', 'bbcode/bbcode.class'))
+{
+    require_once $path;
+    
+    function bbcode_parse($str, $action = NULL, $uid = NULL) {
+	$bbcode = new bbcode;
+	return $bbcode->parse($str, $action, $uid);
+    }
+}
